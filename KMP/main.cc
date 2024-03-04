5f4803526a315e4ee73d9613780ee9cb5cfb723f
ytxmobile98 <5900105+ytxmobile98@users.noreply.github.com>
2024-03-04 10:07:59 +0800

    feat: kmp code

--- KMP/main.cc
+++ KMP/main.cc
@@ -40,6 +40,40 @@ static vector<int> computeLPS(const char* pattern) {
     return lps;
 }
 
+static vector<int> computeKMP(const char* text, const char* pattern) {
+    size_t textLen = strlen(text);
+    size_t patternLen = strlen(pattern);
+    if (textLen < patternLen) {
+        return vector<int>();
+    }
+
+    vector<int> lps = computeLPS(pattern);
+
+    vector<int> result;
+    size_t i = 0;
+    size_t j = 0;
+    while ((textLen - i) >= (patternLen - j)) {
+        if (pattern[j] == text[i]) {
+            ++i;
+            ++j;
+        }
+
+        if (j == patternLen) {
+            result.push_back(i - j); // record the starting position
+            j = lps[j - 1];
+        } else if (i < textLen && (pattern[j] != text[i])) {
+            // mismatch after j matches
+            if (j != 0) {
+                j = lps[j - 1];
+            } else {
+                ++i;
+            }
+        }
+    }
+
+    return result;
+}
+
 int main(int argc, char** argv) {
     if (argc != 3) {
         cout << "Usage: " << argv[0] << " <text> <pattern>" << endl;
@@ -49,8 +83,8 @@ int main(int argc, char** argv) {
     const char* text = argv[1];
     const char* pattern = argv[2];
 
-    vector<int> lps = computeLPS(pattern);
-    cout << lps << endl;
+    vector<int> result = computeKMP(text, pattern);
+    cout << result << endl;
 
     return 0;
 }
